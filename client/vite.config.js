da65e78f04fa75e7a1d2b61f8766720adff0c354
Prakriti Nigam <prakritinigam319@gmail.com>
2025-05-13 20:49:45 +0530

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Text-Editor/",
+  base: "/",
   plugins: [react()],
   server: {
     open: true,
